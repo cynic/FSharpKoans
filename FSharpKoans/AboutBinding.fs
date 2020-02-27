8144c804e0fc0d1468b77762cecd270831c671b5
David Kondic <G18K1405@ict.ru.ac.za>
2020-02-27 15:32:46 +0200

    Updated AboutTesting tests and some of AboutBinding tests

--- FSharpKoans/AboutBinding.fs
+++ FSharpKoans/AboutBinding.fs
@@ -60,21 +60,21 @@ module ``02: About Binding`` =
     [<Test>]
     let ``01 Basic 'let' binding`` () = 
         let x = 50 in // note that the syntax is more explicit about what's really going on!
-            x |> should equal __
+            x |> should equal 50
 
     [<Test>]
     let ``02 Equivalent basic 'let' binding`` () = // this is exactly equivalent to the previous binding.
         let x = 50
-        x |> should equal __
+        x |> should equal 50
 
     [<Test>]
     let ``03 There are many types of values`` () =
-        let a = __
-        let b = __
-        let c = __
-        let d = __
-        let e = __
-        let f = __
+        let a = 2
+        let b = 2.0
+        let c = true
+        let d = "string"
+        let e = 'h'
+        let f = ()
         a |> should be ofType<int>
         b |> should be ofType<float>
         c |> should be ofType<bool>
@@ -84,16 +84,16 @@ module ``02: About Binding`` =
 
     [<Test>]
     let ``04 We can compare values using F#'s comparison operators`` () =
-        1 |???| 2 |> should equal true // change the |???| to be an actual operator
-        2 |???| 1 |> should equal true
-        1 |???| 1 |> should equal true
-        1 |???| 1 |> should equal false
-        () |???| () |> should equal true
-        () |???| () |> should equal false
+        1 < 2 |> should equal true // change the |???| to be an actual operator
+        2 > 1 |> should equal true
+        1 = 1 |> should equal true
+        1 <> 1 |> should equal false
+        () = () |> should equal true
+        () <> () |> should equal false
 
     [<Test>]
     let ``05 There's a `not` function instead of a `not` operator`` () =
-        __ true |> should equal false
+        not true |> should equal false
 
     [<Test>]
     let ``06 Nest your 'let' statements as deeply as you'd like`` () =
@@ -103,7 +103,7 @@ module ``02: About Binding`` =
                     let d = 63 in d
                 c + 1
             b + 7
-        a |> should equal ___
+        a |> should equal 71
 
 (*
     Identifiers are *referentially transparent*: the link between value and identifier never changes.
@@ -139,9 +139,9 @@ module ``02: About Binding`` =
             3 + a
         let c = a + 4
         let a = a + a
-        a |> should equal __
-        b |> should equal __
-        c |> should equal __
+        a |> should equal 42
+        b |> should equal 11
+        c |> should equal 25
 
    
    (*
